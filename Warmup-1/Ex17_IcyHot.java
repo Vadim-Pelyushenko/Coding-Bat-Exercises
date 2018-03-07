f9576955591829047033dded645d41e912a2caee
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 03:38:52 -0800

    The 17th Exercise
    I am not sure whether or not there's a more concise way to do this, feels a little verbose

---
+++ Warmup-1/Ex17_IcyHot.java
@@ -0,0 +1,9 @@
+
+public class Ex17_IcyHot
+{
+	// Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+	public boolean icyHot(int temp1, int temp2)
+	{
+		return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+	}
+}
