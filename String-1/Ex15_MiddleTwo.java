dcab3980e6dc2ca168a7c0a18af242803553b55c
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 13:17:55 -0800

    The 15th Exercise
    Not a one-liner

---
+++ String-1/Ex15_MiddleTwo.java
@@ -0,0 +1,11 @@
+
+public class Ex15_MiddleTwo
+{
+	// Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+	// The string length will be at least 2.
+	public String middleTwo(String str)
+	{
+		int mid = str.length() / 2 - 1;
+		return str.substring(mid, mid + 2);
+	}
+}
