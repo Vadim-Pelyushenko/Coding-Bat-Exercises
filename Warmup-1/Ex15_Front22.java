ae2538ba6c2a234153ea661924842b29d7350a6e
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 03:33:59 -0800

    Simplification of Structure
    Much like another past problem, changed an if/else statement for a ternary

--- Warmup-1/Ex15_Front22.java
+++ Warmup-1/Ex15_Front22.java
@@ -5,11 +5,7 @@ public class Ex15_Front22
 	// so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
 	public String front22(String str)
 	{
-		String front;
-		if (str.length() < 2)
-			front = str;
-		else
-			front = str.substring(0, 2);
+		String front = str.length() < 2 ? str : str.substring(0,2);
 		return front + str + front;
 	}
 }
