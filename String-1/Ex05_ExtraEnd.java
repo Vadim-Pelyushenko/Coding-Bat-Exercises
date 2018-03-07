669f332b43fe1b33d608b5ebf1ff2ccf8ea1f8a4
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 12:58:46 -0800

    The 5th Exercise

---
+++ String-1/Ex05_ExtraEnd.java
@@ -0,0 +1,11 @@
+
+public class Ex05_ExtraEnd
+{
+	// Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string
+	// length will be at least 2.
+	public String extraEnd(String str)
+	{
+		String end = str.substring(str.length() - 2);
+		return end + end + end;
+	}
+}
