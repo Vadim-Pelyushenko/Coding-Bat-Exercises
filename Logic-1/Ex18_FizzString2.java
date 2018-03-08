dac23efd5600ba86f484b3797f97d9d5e15498da
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 19:54:45 -0800

    The 18th Exercise
    Closer to the commonly thought of FizzBuzz problem

---
+++ Logic-1/Ex18_FizzString2.java
@@ -0,0 +1,18 @@
+
+public class Ex18_FizzString2
+{
+	// Given an int n, return the string form of the number followed by "!". So the int 6 yields "6!". Except if the
+	// number is divisible by 3 use "Fizz" instead of the number, and if the number is divisible by 5 use "Buzz", and if
+	// divisible by both 3 and 5, use "FizzBuzz". Note: the % "mod" operator computes the remainder after division, so
+	// 23 % 10 yields 3. What will the remainder be when one number divides evenly into another? (See also: FizzBuzz
+	// Code and Introduction to Mod)
+	public String fizzString2(int n)
+	{
+		String result = "";
+		if (n % 3 == 0)
+			result += "Fizz";
+		if (n % 5 == 0)
+			result += "Buzz";
+		return result.length() == 0 ? n + "!" : result + "!";
+	}
+}
