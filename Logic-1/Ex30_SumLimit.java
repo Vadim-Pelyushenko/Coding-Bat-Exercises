7ef045fd1cd5f0eba754f36f68ebee07b120da5a
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 20:29:41 -0800

    The 30th Exercise
    When I wanted to "cast" something as a String, I used to do (a + ""), using String.valueOf() seems to be more reasonable

---
+++ Logic-1/Ex30_SumLimit.java
@@ -0,0 +1,12 @@
+
+public class Ex30_SumLimit
+{
+	// Given 2 non-negative ints, a and b, return their sum, so long as the sum has the same number of digits as a. If
+	// the sum has more digits than a, just return a without b. (Note: one way to compute the number of digits of a
+	// non-negative int n is to convert it to a string with String.valueOf(n) and then check the length of the string.)
+	public int sumLimit(int a, int b)
+	{
+		int sum = a + b;
+		return String.valueOf(sum).length() == String.valueOf(a).length() ? sum : a;
+	}
+}
