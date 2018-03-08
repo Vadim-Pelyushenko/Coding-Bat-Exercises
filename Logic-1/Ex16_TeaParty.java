0c4c1dee92a0ae507dac31736557451f0975f613
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-07 19:43:36 -0800

    The 16th Exercise
    Its lit

---
+++ Logic-1/Ex16_TeaParty.java
@@ -0,0 +1,14 @@
+
+public class Ex16_TeaParty
+{
+	// We are having a party with amounts of tea and candy. Return the int outcome of the party encoded as 0=bad,
+	// 1=good, or 2=great. A party is good (1) if both tea and candy are at least 5. However, if either tea or candy is
+	// at least double the amount of the other one, the party is great (2). However, in all cases, if either tea or
+	// candy is less than 5, the party is always bad (0).
+	public int teaParty(int tea, int candy)
+	{
+		if (candy < 5 || tea < 5)
+			return 0;
+		return candy >= 2 * tea || tea >= 2 * candy ? 2 : 1;
+	}
+}
