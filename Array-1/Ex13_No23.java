638970b898a47a126a81c97fc957ed7d1d714052
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-08 22:11:50 -0800

    The 13th Exercise
    Could I have copied the previous exercise and inverted the output?
    Yes
    But I want my code to be as short as possible, so I applied DeMorgans

---
+++ Array-1/Ex13_No23.java
@@ -0,0 +1,9 @@
+
+public class Ex13_No23
+{
+	// Given an int array length 2, return true if it does not contain a 2 or 3.
+	public boolean no23(int[] nums)
+	{
+		return nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3;
+	}
+}
