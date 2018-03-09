057d7fd26e370318cda1b94303850733b5d25284
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-08 22:09:44 -0800

    The 12th Exercise

---
+++ Array-1/Ex12_Has23.java
@@ -0,0 +1,9 @@
+
+public class Ex12_Has23
+{
+	// Given an int array length 2, return true if it contains a 2 or a 3.
+	public boolean has23(int[] nums)
+	{
+		return nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3;
+	}
+}
