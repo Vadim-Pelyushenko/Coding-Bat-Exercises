9e5427ece5883b6772b71dc30e6122b81ffc6d7e
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-08 22:39:15 -0800

    Changed the code to do it the proper way

--- Array-1/Ex24_FrontPiece.java
+++ Array-1/Ex24_FrontPiece.java
@@ -5,6 +5,14 @@ public class Ex24_FrontPiece
 	// 2, use whatever elements are present.
 	public int[] frontPiece(int[] nums)
 	{
-		return nums.length < 2 ? nums : new int[] { nums[0], nums[1] };
+		switch (nums.length)
+		{
+			case 0:
+				return new int[] {};
+			case 1:
+				return new int[] { nums[0] };
+			default:
+				return new int[] { nums[0], nums[1] };
+		}
 	}
 }
