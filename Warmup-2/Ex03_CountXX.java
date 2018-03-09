9cf369b46b6c4a3b7eeb33619cf3bf5926214bdf
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-09 15:48:42 -0800

    The 3rd Exercise

---
+++ Warmup-2/Ex03_CountXX.java
@@ -0,0 +1,15 @@
+
+public class Ex03_CountXX
+{
+	// Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+	int countXX(String str)
+	{
+		int t = 0;
+		for (int i = 0; i < str.length() - 1; i++)
+		{
+			if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x')
+				t++;
+		}
+		return t;
+	}
+}
