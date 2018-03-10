cb9d5a56bb30fc921323ed928b09231b52920139
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-09 16:00:54 -0800

    The 6th Exercise

---
+++ Warmup-2/Ex06_StringSplosion.java
@@ -0,0 +1,12 @@
+
+public class Ex06_StringSplosion
+{
+	// Given a non-empty string like "Code" return a string like "CCoCodCode".
+	public String stringSplosion(String str)
+	{
+		String result = "";
+		for (int i = 1; i <= str.length(); i++)
+			result += str.substring(0, i);
+		return result;
+	}
+}
