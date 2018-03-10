2f0a03870ad125f0bd0700be73d47c248a0af4c9
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-10 15:32:45 -0800

    The 6th Exercise

---
+++ String-2/Ex06_XYZThere.java
@@ -0,0 +1,17 @@
+
+public class Ex06_XYZThere
+{
+	// Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a
+	// period (.). So "xxyz" counts but "x.xyz" does not.
+	public boolean xyzThere(String str)
+	{
+		for (int i = 0; i < str.length() - 2; i++)
+		{
+			if (str.charAt(i) == '.')
+				i++;
+			else if (str.substring(i, i + 3).equals("xyz"))
+				return true;
+		}
+		return false;
+	}
+}
