966da52528fa113c037198d36b000f2c6aad787a
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-10 15:48:07 -0800

    The 8th Exercise

---
+++ String-2/Ex08_XYBalance.java
@@ -0,0 +1,18 @@
+
+public class Ex08_XYBalance
+{
+	// We'll say that a String is xy-balanced if for all the 'x' chars in the string, there exists a 'y' char somewhere
+	// later in the string. So "xxy" is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true if
+	// the given string is xy-balanced.
+	public boolean xyBalance(String str)
+	{
+		for (int i = str.length() - 1; i >= 0; i--)
+		{
+			if (str.charAt(i) == 'y')
+				return true;
+			if (str.charAt(i) == 'x')
+				return false;
+		}
+		return true;
+	}
+}
