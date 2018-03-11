bf17207725e541961b7981ad380fda8294d833e7
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-10 16:47:14 -0800

    The 18th Exercise

---
+++ String-2/Ex18_ZipZap.java
@@ -0,0 +1,22 @@
+
+public class Ex18_ZipZap
+{
+	// Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a
+	// string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+	public String zipZap(String str)
+	{
+		String result = "";
+		int i;
+		for (i = 0; i < str.length() - 2; i++)
+		{
+			if (str.charAt(i) == 'z' && str.charAt(i + 2) == 'p')
+			{
+				result += "zp";
+				i += 2;
+			}
+			else
+				result += str.charAt(i);
+		}
+		return result + str.substring(i);
+	}
+}
