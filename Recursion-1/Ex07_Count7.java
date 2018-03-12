4ad24f9147859fa9b6a17da68fca4c0b73c605af
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-12 14:52:57 -0700

    The 7th Exercise

---
+++ Recursion-1/Ex07_Count7.java
@@ -0,0 +1,13 @@
+
+public class Ex07_Count7
+{
+	// Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no
+	// loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the
+	// rightmost digit (126 / 10 is 12).
+	public int count7(int n)
+	{
+		if (n == 0)
+			return 0;
+		return n % 10 == 7 ? count7(n / 10) + 1 : count7(n / 10);
+	}
+}
