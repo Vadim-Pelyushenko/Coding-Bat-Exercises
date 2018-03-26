374838adc45d5632eae279ff6d886a883411013c
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-26 16:48:37 -0700

    The 7th Exercise

---
+++ Map 1/Ex07_MapAB2.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+
+public class Ex07_MapAB2
+{
+	// Modify and return the given map as follows: if the keys "a" and "b" are both in the map and have equal values,
+	// remove them both.
+	public Map<String, String> mapAB2(Map<String, String> map)
+	{
+		if (map.containsKey("a") && map.containsKey("b") && map.get("a").equals(map.get("b")))
+		{
+			map.remove("a");
+			map.remove("b");
+		}
+		return map;
+	}
+}
