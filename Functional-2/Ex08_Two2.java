0af33a17d5e4d8e6fdbadb5f65d7f0a0f9fe60c2
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-27 15:40:32 -0700

    The 8th Exercise

---
+++ Functional-2/Ex08_Two2.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Ex08_Two2
+{
+	// Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting any of the
+	// resulting numbers that end in 2.
+	public List<Integer> two2(List<Integer> nums)
+	{
+		nums.replaceAll(n -> n * 2);
+		nums.removeIf(n -> n % 10 == 2);
+		return nums;
+	}
+}
