448c30e61f53c892e85a3cdefed3edf140dd6c33
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-27 15:33:49 -0700

    The First Functional-2 Exercise

---
+++ Functional-2/Ex01_NoNeg.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Ex01_NoNeg
+{
+	// Given a list of integers, return a list of the integers, omitting any that are less than 0.
+	public List<Integer> noNeg(List<Integer> nums)
+	{
+		nums.removeIf(n -> n < 0);
+		return nums;
+	}
+}
