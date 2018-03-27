1ec476e14ec94ae34a66dbd8c93c5053d3f22de6
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-27 15:27:12 -0700

    The 6th Exercise

---
+++ Functional-1/Ex06_Math1.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Ex06_Math1
+{
+	// Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+	public List<Integer> math1(List<Integer> nums)
+	{
+		nums.replaceAll(n -> (n + 1) * 10);
+		return nums;
+	}
+}
