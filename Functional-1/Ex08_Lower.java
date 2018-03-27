ef2e24b3724a44a40a1e5e423704143156420b9c
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-27 15:28:53 -0700

    The 8th Exercise

---
+++ Functional-1/Ex08_Lower.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Ex08_Lower
+{
+	// Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase()
+	// method).
+	public List<String> lower(List<String> strings)
+	{
+		strings.replaceAll(s -> s.toLowerCase());
+		return strings;
+	}
+}
